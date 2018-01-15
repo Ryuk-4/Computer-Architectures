9dfbd32fc203d758dd38efa266e2d6bf5067d91b
Ryuk-4 <vito.damaso@gmail.com>
2018-01-15 23:53:15 +0100

    Add tabs

--- ARM-labs/Problem_6_2017/lab6_3.s
+++ ARM-labs/Problem_6_2017/lab6_3.s
@@ -12,29 +12,10 @@ reset_handler
 		LDR R6, =7
 		LDR R7, =8
 
-		CMP R0,R1
-	;R8 = R0 + R1 if R0 != R1
-		ADDNE R8, R0, R1
-		LSRNE R8, R8, #1
-		MULEQ R8, R0, R1
-
-		LDR R0, =5
-		LDR R1, =2
-		LDR R2, =4
-
-		CMP R0, R1
-		MOVHI R4, R0
-		MOVHI R0, R1
-		MOVHI R1, R4
-
-		CMP R1, R2
-		MOVHI R4, R1
-		MOVHI R1, R2
-		MOVHI R2, R4
 
 
 
 
 
 	B reset_handler
-	END
+		END
